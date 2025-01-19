0a846e62f209f868350185b6a889213b4ea34989
Thomas <thomas.popp@cariad.technology>
2025-01-19 23:31:57 +0100

    build(esp32-devkit): apply suggestions from community

--- AquaMQTT/include/config/Configuration.h
+++ AquaMQTT/include/config/Configuration.h
@@ -137,11 +137,21 @@ constexpr uint16_t MQTT_MAX_PAYLOAD_SIZE  = 1024;
 
 /**
  * Pin assignments for AquaMQTT Board Rev 1.0
+ * There are different GPIO ports depending on the hardware.
+ * The ENV_DEVKIT_ESP32 macro is set by the platformio environment.
+ * By default we use the pin assignments of the Arduino Nano ESP32
  */
+#ifdef ENV_DEVKIT_ESP32
+constexpr uint8_t GPIO_MAIN_RX = 5;
+constexpr uint8_t GPIO_MAIN_TX = 6;
+constexpr uint8_t GPIO_HMI_RX  = 7;
+constexpr uint8_t GPIO_HMI_TX  = 8;
+#else
 constexpr uint8_t GPIO_MAIN_RX = 2;
 constexpr uint8_t GPIO_MAIN_TX = 3;
 constexpr uint8_t GPIO_HMI_RX  = 4;
 constexpr uint8_t GPIO_HMI_TX  = 5;
+#endif
 constexpr uint8_t GPIO_SDA_RTC = A4;
 constexpr uint8_t GPIO_SCL_RTC = A5;
 
